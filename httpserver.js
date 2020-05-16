68d797967bf24e0954a71a57e65b0bfbba9b4290
codesprintpro <sachin.sarawgi@servicenow.com>
2020-05-16 23:24:40 +0530

    Setting response header for all the request method type.

--- httpserver.js
+++ httpserver.js
@@ -2,6 +2,7 @@ const http = require('http');
 const REQUIRED_CONTENT_TYPE = 'application/json';
 const ACCEPT_ENCODING_1 = 'application/json';
 const ACCEPT_ENCODING_2 = '*/*';
+const GET_URL_FORMAT = "";
 
 
 const requestListener = function (req, res) {
@@ -17,18 +18,22 @@ const requestListener = function (req, res) {
     console.log(Object.getOwnPropertyNames(req));
     switch(methodType){
         case 'GET':
+            prepareResponseHeaderObject(res);
             res.writeHead(200);
             res.end(`The request method type is ${methodType}`);
             break;
         case 'POST':
+            prepareResponseHeaderObject(res);
             res.writeHead(200);
             res.end(`The request method type is ${methodType}`);
             break;
         case 'PUT':
+            prepareResponseHeaderObject(res);
             res.writeHead(200);
             res.end(`The request method type is ${methodType}`);
             break;
         case 'DELETE':
+            prepareResponseHeaderObject(res);
             res.writeHead(200);
             res.end(`The request method type is ${methodType}`);
             break;
@@ -45,6 +50,11 @@ const entryCheck = function(req){
     if(!(accept.includes(ACCEPT_ENCODING_1) || accept.includes(ACCEPT_ENCODING_2))){
         throw new Error("Sorry we only support accept json format.");
     }
+}
+
+const prepareResponseHeaderObject = function(res){
+    res.setHeader('Content-Type', REQUIRED_CONTENT_TYPE);
+    res.setHeader( 'Cache-Control', 'no-cache');
 }
 
 const server = http.createServer(requestListener);
